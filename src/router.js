fd272602d70c0b015ca6b3d1226358bb734a279e
Your Name <you@example.com>
2019-05-06 11:05:09 +0800

    增加login组件

---
+++ src/router.js
@@ -0,0 +1,21 @@
+// 导入vue
+import Vue from 'vue'
+// 导入VueRouter
+import VueRouter from 'vue-router'
+// use一下
+Vue.use(VueRouter)
+// 导入组件
+import login from './components/login.vue'
+// 路由规则
+const routes=[
+    {
+        path:'/login',
+        component:login
+    }
+]
+// 实例化路由对象
+const router=new VueRouter({
+    routes
+})
+// 暴露出去
+export default router 
